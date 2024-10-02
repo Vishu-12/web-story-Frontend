ea4a6e345f0c22ebed1b07977c3e541df0a05574
Vishu-12 <garg.vishu810@gmail.com>
2024-10-02 23:00:55 +0530

    chnage gitignore file

---
+++ src/env.js
@@ -0,0 +1,7 @@
+export const baseUrl = "https://web-story-backend.onrender.com";
+
+export const apiEndPoint = {
+  login: "/api/auth/login",
+  register: "/api/auth/register",
+  story: "/api/story",
+};
